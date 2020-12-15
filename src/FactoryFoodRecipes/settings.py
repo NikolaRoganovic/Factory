7a399494396b53ccd4347c00a54e145503f655db
NikolaRoganovic <dzonixd@gmail.com>
2020-12-15 18:15:31 +0100

    Settings and base setup on homepage

--- src/FactoryFoodRecipes/settings.py
+++ src/FactoryFoodRecipes/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'accounts',
+    'recipes',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +56,7 @@ ROOT_URLCONF = 'FactoryFoodRecipes.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['FactoryFoodRecipes/templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
